38f4216af9977376afe69eafac13fba4ce32dca1
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2025-04-12 18:48:40 +0330

    update routing system

--- system/Rputer/web/Route.php
+++ system/Rputer/web/Route.php
@@ -13,17 +13,23 @@ class Route {
         $class = $executeMethod[0];
         $method = $executeMethod[1];
         global $routes ;
+        array_push($routes['post'],array('url' => trim($url , "/ ") , 'class' => $class , 'method' => $method , 'name' => $name));
+
      }
      public static function put($url , $executeMethod , $name = null){
         $executeMethod =  explode('@' , $executeMethod);
         $class = $executeMethod[0];
         $method = $executeMethod[1];
-        global $routes ;
+        global $routes ;    
+           array_push($routes['put'],array('url' => trim($url , "/ ") , 'class' => $class , 'method' => $method , 'name' => $name));
+
      }
      public static function delete($url , $executeMethod , $name = null){
         $executeMethod =  explode('@' , $executeMethod);
         $class = $executeMethod[0];
         $method = $executeMethod[1];
         global $routes ;
+        array_push($routes['delete'],array('url' => trim($url , "/ ") , 'class' => $class , 'method' => $method , 'name' => $name));
+
      }
 }
