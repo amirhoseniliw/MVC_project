67497d176e82ad63003e3ab1d26985bc60438810
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2025-04-13 14:20:38 +0330

    update routing system

--- system/Router/Routing.php
+++ system/Router/Routing.php
@@ -10,6 +10,20 @@ class Routing {
         $this->current_route = explode('/' , CURRENT_ROUTE);
         $this->method_field = $this->methodField();
         global $routes ;
-        $this->routes =  $routes ;
+        $this->routes = $routes ;
+    }
+    public function methodField(){
+
+        $method_field = strtolower($_SERVER['REQUEST_METHOD']);
+        if ($method_field == 'post') {
+           if(isset($_POST['_method'])){
+            if($_POST['_method'] == 'put'){
+                $method_field = 'put';
+            }
+            elseif($_POST['_method'] == 'delete'){
+                $method_field = 'delete';
+            }
+        }}
+        return $method_field ;
     }
 }
