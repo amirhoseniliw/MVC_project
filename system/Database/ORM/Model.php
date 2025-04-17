f7a3b6a5b09e188ad2347a1783ea044473b52cc4
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2025-04-17 12:24:47 +0330

    update DB system

--- system/Database/ORM/Model.php
+++ system/Database/ORM/Model.php
@@ -1,1 +1,11 @@
 <?php
+namespace System\Database\ORM;
+use System\Database\Traits\HasAtrbutes ;
+use System\Database\Traits\HasCRUD ;
+use System\Database\Traits\HasMethodCaller ;
+use System\Database\Traits\HasQueryBuilder ;
+use System\Database\Traits\HasRelation ;
+
+abstract class Model {
+    use HasAtrbutes , HasCRUD , HasMethodCaller , HasQueryBuilder , HasRelation  ;
+}
---
+++ system/Database/Traits/HasAtrbutes.php
@@ -0,0 +1,2 @@
+<?php 
+namespace System\Database\Traits;
