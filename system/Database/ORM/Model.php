d12f9ba609e24750789952cf643a6e9cf2e90f13
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2025-04-26 22:20:47 +0330

    update DB system

--- system/Database/ORM/Model.php
+++ system/Database/ORM/Model.php
@@ -1,6 +1,6 @@
 <?php
 namespace System\Database\ORM;
-use System\Database\Traits\HasAtrbutes ;
+use System\Database\Traits\HasAttributes ;
 use System\Database\Traits\HasCRUD ;
 use System\Database\Traits\HasMethodCaller ;
 use System\Database\Traits\HasQueryBuilder ;
--- system/Database/Traits/HasCRUD.php
+++ system/Database/Traits/HasCRUD.php
@@ -1,5 +1,28 @@
-<?php 
+<?php
+
 namespace System\Database\Traits;
-trait HasCRUD {
+
+use System\Database\DBConnection\DBConnection;
+
+trait HasCRUD{
+
+
+
+
+    protected function fill(){
+
+        $fillArray = array();
+        foreach ($this->fillable as $attribute){
+            if(isset($this->$attribute)){
+                array_push($fillArray, $this->getAttributeName($attribute) . " = ?");
+                $this->inCastsAttributes($attribute) == true ? $this->addValue($attribute, $this->castEncodeValue($attribute, $this->$attribute)) : $this->addValue($attribute, $this->$attribute);
+            }
+        }
+        $fillString = implode(', ', $fillArray);
+        return $fillString;
+    }
+
+
     
+
 }
