4f4c72450d9ca1db360b4066cea0a75fb1c8f614
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2025-05-02 14:19:34 +0330

    upadate

--- system/Database/Traits/HasCRUD.php
+++ system/Database/Traits/HasCRUD.php
@@ -5,7 +5,17 @@ namespace System\Database\Traits;
 use System\Database\DBConnection\DBConnection;
 
 trait HasCRUD{
-
+    protected function createMethod($values){
+        $values = $this->arrayToCastEncodeValue($values);
+        $this->arrayToAttributes($values, $this);
+        return $this->saveMethod();
+    }
+    
+    protected function updateMethod($values){
+        $values = $this->arrayToCastEncodeValue($values);
+        $this->arrayToAttributes($values, $this);
+        return $this->saveMethod();
+    }
         
     protected function deleteMethod($id = null){
 
