c4354b0a95190f692ca55a462c1caa84b903c692
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2025-05-03 22:35:46 +0330

    update

--- system/Database/Traits/HasRelation.php
+++ system/Database/Traits/HasRelation.php
@@ -25,4 +25,45 @@ trait HasRelation
         if ($data)
         return $this->arrayToAttributes($data);
          return null;
-    }}
+    }
+    protected function hasMany($model, $foreignKey, $otherKey){
+        if($this->{$this->primaryKey}){
+            $modelObject = new $model;
+            return $modelObject->getHasManyRelation($this->table, $foreignKey, $otherKey, $this->$otherKey);
+        }
+    }
+
+    public function getHasManyRelation($table, $foreignKey, $otherKey, $otherKeyValue){
+        // sql = 'SELECT posts.* FROM categories JOIN posts ON categories.id = posts.cat_id'
+        // sql = 'SELECT categories.* FROM categories JOIN categories ON categories.id = categories.parent_id'
+        $this->setSql("SELECT `b`.* FROM `{$table}` AS `a` JOIN ".$this->getTableName()." AS `b` on `a`.`{$otherKey}` = `b`.`{$foreignKey}` ");
+        $this->setWhere('AND', "`a`.`$otherKey` = ? ");
+        $this->table = 'b';
+        $this->addValue($otherKey, $otherKeyValue);
+       return $this;
+    }
+
+
+    protected function belongsTo($model, $foreignKey, $localKey){
+        if($this->{$this->primaryKey}){
+            $modelObject = new $model;
+            return $modelObject->getBelongsToRelation($this->table, $foreignKey, $localKey, $this->$foreignKey);
+        }
+    }
+
+    public function getBelongsToRelation($table, $foreignKey, $otherKey, $foreignKeyValue){
+        // sql = 'SELECT posts.* FROM categories JOIN posts ON categories.id = posts.cat_id'
+        $this->setSql("SELECT `b`.* FROM `{$table}` AS `a` JOIN ".$this->getTableName()." AS `b` on `a`.`{$foreignKey}` = `b`.`{$otherKey}` ");
+        $this->setWhere('AND', "`a`.`$foreignKey` = ? ");
+        $this->table = 'b';
+        $this->addValue($foreignKey, $foreignKeyValue);
+        $statement = $this->executeQuery();
+        $data = $statement->fetch();
+        if ($data)
+        return $this->arrayToAttributes($data);
+         return null;
+    }
+
+
+
+}
